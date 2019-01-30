25837cd91dc4c4cf1e3e84e81ef4dc39109eabd7
Matteo Locher <matteolocher@gmail.com>
2019-01-30 20:53:49 +0100

    Remove uncommented code

--- node/carbone.js
+++ node/carbone.js
@@ -1,4 +1,3 @@
-// const fs = require('fs');
 const carbone = require('carbone');
 
 module.exports = {
@@ -6,13 +5,8 @@ module.exports = {
     var data = JSON.parse(dataIn);
     carbone.render('./node_modules/carbone/examples/simple.odt', data, function (err, result) {
       if (err) {
-        console.log(err);
         callback(err, null);
       }
-      // write the result to the filesystem
-      // fs.writeFileSync('result.odt', result);
-      
-      // return the result
       callback(null, result);
     });
   }
